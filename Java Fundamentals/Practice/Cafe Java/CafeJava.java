beef21e4c2f5dbd45f3c369c0c56b39f8fd0a381
MehdiMahmoudico <mmeehdi.mma@gmail.com>
2024-09-26 23:55:20 +0100

    cafe

--- Java Fundamentals/Practice/Cafe Java/CafeJava.java
+++ Java Fundamentals/Practice/Cafe Java/CafeJava.java
@@ -20,17 +20,36 @@ public class CafeJava {
         String customer4 = "Jimmy";
     
         // Order completions (add yours below)
-        boolean isReadyOrder1 = false;
+        boolean isReadyOrder = true;
     
 
-        if(isReadyOrder1){
-            System.out.println(readyMessage);
+        if(isReadyOrder){
+            System.out.println(customer1 +readyMessage);
         }else{
-            System.out.println(pendingMessage);
+            System.out.println(customer1 + pendingMessage);
         }
         // APP INTERACTION SIMULATION (Add your code for the challenges below)
         // Example:
         System.out.println(generalGreeting + customer1); // Displays "Welcome to Cafe Java, Cindhuri"
     	// ** Your customer interaction print statements will go here ** //
+        if(isReadyOrder){
+            System.out.println(customer3 + readyMessage );
+            System.out.println( displayTotalMessage + (cappuccino));
+        }else{
+            System.out.println(customer3 + pendingMessage);
+        }
+        if(isReadyOrder){
+            System.out.println(customer2 + readyMessage );
+            System.out.println( displayTotalMessage + (2 * latte));
+        }else{
+            System.out.println(customer2 + pendingMessage);
+        }
+        if(isReadyOrder){
+            System.out.println(customer4 + readyMessage );
+            System.out.println( displayTotalMessage + (latte - dripcoffe));
+        }else{
+            System.out.println(customer4 + pendingMessage);
+        }
+        
     }
 }
